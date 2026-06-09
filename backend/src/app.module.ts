54724795d705f6e83ac83322d211cc39be936832
A01749746 <a01749746@tec.mx>
2026-06-09 12:03:47 -0600

    scaffold Postgres + Prisma infra

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ModelController } from './model/model.controller';
 import { ModelService } from './model/model.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [ModelController],
   providers: [ModelService],
 })
